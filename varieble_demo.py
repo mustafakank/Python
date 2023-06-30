04ee880b05205fd3199894b0f389f0fa02023f36
mustafakank <mustafakank@outlook.com>
2023-07-01 01:21:40 +0300

    Veriables

---
+++ varieble_demo.py
@@ -0,0 +1,16 @@
+musteri_ad = "Mustafa"
+musteri_soyad = "Kılıçsokan"
+musteri_ad_soyad = musteri_ad + musteri_soyad
+musteri_cinsiyet = "Erkek"
+musteri_tc = 25661693064
+musteri_doğum_yılı = 2005
+musteri_adres = "Cumhuriyet Mah./Namık Kemal Cad./No:68"
+musteri_yas = 17
+print(musteri_ad, musteri_soyad, musteri_ad_soyad, musteri_cinsiyet, musteri_tc, musteri_doğum_yılı, musteri_adres, musteri_yas)
+
+
+sprs1 = 110
+sprs2 = 1100.5
+sprs3 = 356.95
+toplam = (sprs1 + sprs2 + sprs3)
+print("Toplam: " + toplam)
