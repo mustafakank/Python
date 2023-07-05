dc1e2c6f2a9920dca2c4e716ef55f66f0f7d21e5
Mustafa <mustafakank@outlook.com>
2023-07-05 23:58:50 +0300

    05.06.2023

--- f-strings.py
+++ f-strings.py
@@ -1,14 +1,12 @@
-a = 5
-b = 10
+a , b = 5 , 10
 print(f"{a} ile {b}'u çarparsak {a * b}'yi elde ederiz.")
 
-# name = "Mustafa"
-# surname = "Kılıçsokan"
-# age = 17
-# print(f"Benim adım {name}, soyadım {surname}. {age} yaşında bir lise mezunuyum.")
-
-name = input("adınız: ")
-surname = input("soyadınız: ")
-age = input("yaşınız: ")
-
+name , surname , age = input("adınız: "), input("soyadınz: "), input("yaşınız: ")
 print(f"Merhaba {name} {surname} bey/bayan. Nasılsınız? {age} yaşında olmak nasıl bir duygu? ")
+
+# değişkenin başına " * " koyarsak diğer her değişkene 1 karakter atanırken *'lı değişkene diğerlerinden kalan bütün karakterler atanır.
+fiyat = "€1456,789#"
+ilk, *orta, son = fiyat
+print(ilk)
+print(*orta, sep="")
+print(son)
