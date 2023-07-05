91fbc61750d142efc645988721146f53e89811c6
Mustafa <mustafakank@outlook.com>
2023-07-05 23:04:18 +0300

    05.06.2023

---
+++ f-strings.py
@@ -0,0 +1,14 @@
+a = 5
+b = 10
+print(f"{a} ile {b}'u çarparsak {a * b}'yi elde ederiz.")
+
+# name = "Mustafa"
+# surname = "Kılıçsokan"
+# age = 17
+# print(f"Benim adım {name}, soyadım {surname}. {age} yaşında bir lise mezunuyum.")
+
+name = input("adınız: ")
+surname = input("soyadınız: ")
+age = input("yaşınız: ")
+
+print(f"Merhaba {name} {surname} bey/bayan. Nasılsınız? {age} yaşında olmak nasıl bir duygu? ")
